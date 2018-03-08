384dd05865faad5c9041b7936b3943f46f0069d6
BobbyRockers <rrockers@gmail.com>
2018-03-07 22:53:23 -0600

    Make it look a little nicer

--- countNumbers.cpp
+++ countNumbers.cpp
@@ -7,14 +7,15 @@ int main(){
 	
 	for(int x=0; x<10; x++){
 		for( int y=0; y<10; y++){
-			numbers[x][y] = 'X';
+			numbers[x][y] = '~';
 		}
 	}
 	
-	cout << endl << "      0 1 2 3 4 5 6 7 8 9" << endl;
+	cout << endl << "       0 1 2 3 4 5 6 7 8 9" << endl;
+	cout        <<  "       ____________________" << endl;
 	
 	for(int x=0; x<10; x++){
-		cout << "    " << x << " ";
+		cout << "    " << x << " | ";
 		for( int y=0; y<10; y++){
 			cout << numbers[x][y] << " ";
 		}
