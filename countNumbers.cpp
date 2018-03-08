6d21b97af123072704255fd70a8caa74e549d58f
BobbyRockers <rrockers@gmail.com>
2018-03-07 21:01:19 -0600

    All things arrays

---
+++ countNumbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	char numbers[10][10];
+	
+	for(int x=0; x<10; x++){
+		for( int y=0; y<10; y++){
+			numbers[x][y] = 'X';
+		}
+	}
+	
+	cout << endl << "      0 1 2 3 4 5 6 7 8 9" << endl;
+	
+	for(int x=0; x<10; x++){
+		cout << "    " << x << " ";
+		for( int y=0; y<10; y++){
+			cout << numbers[x][y] << " ";
+		}
+		cout << endl;
+	}
+	
+	return 0;
+}
